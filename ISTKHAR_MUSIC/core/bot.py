647b19a58353144c739029665d083d005512b2d7
istu56 <githubop94@gmail.com>
2026-03-10 13:41:07 +0530

    Update bot.py

--- ISTKHAR_MUSIC/core/bot.py
+++ ISTKHAR_MUSIC/core/bot.py
@@ -1,10 +1,8 @@
-import asyncio
 import logging
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 
 from config import API_ID, API_HASH, BOT_TOKEN, LOGGER_ID
-
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,51 +11,32 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger("ISTKHAR_MUSIC.core.bot")
 
-
-class Bot:
-    def __init__(self):
-        self.app = Client(
-            "ISTKHAR_MUSIC_BOT",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            parse_mode=ParseMode.HTML
-        )
-
-    async def start(self):
-        await self.app.start()
-        LOGGER.info("sᴛᴀʀᴛɪɴɢ ʙᴏᴛ...")
-
-        # log group check
-        try:
-            if LOGGER_ID:
-                await self.app.send_message(
-                    LOGGER_ID,
-                    "✅ **ISTKHAR MUSIC BOT STARTED SUCCESSFULLY**"
-                )
-        except Exception as e:
-            LOGGER.error("ʙᴏᴛ ʜᴀs ғᴀɪʟᴇᴅ ᴛᴏ ᴀᴄᴄᴇss ᴛʜᴇ ʟᴏɢ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ.")
-            LOGGER.error(f"Reason :- {type(e).__name__}")
-
-        LOGGER.info("✅ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ")
-
-    async def stop(self):
-        await self.app.stop()
-        LOGGER.info("❌ ʙᴏᴛ sᴛᴏᴘᴘᴇᴅ")
-
-
-bot = Bot()
+# Main Bot Client
+ISTKHAR = Client(
+    "ISTKHAR_MUSIC_BOT",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    parse_mode=ParseMode.HTML
+)
 
 
 async def start_bot():
-    await bot.start()
+    await ISTKHAR.start()
+    LOGGER.info("🚀 ISTKHAR MUSIC BOT STARTED")
+
+    # Log group message (error aaye to ignore karega)
+    try:
+        if LOGGER_ID:
+            await ISTKHAR.send_message(
+                LOGGER_ID,
+                "✅ **ISTKHAR MUSIC BOT START HO GAYA**"
+            )
+    except Exception as e:
+        LOGGER.error("Bot log group access nahi kar pa raha.")
+        LOGGER.error(f"Reason: {type(e).__name__}")
 
 
 async def stop_bot():
-    await bot.stop()
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_bot())
-    loop.run_forever()
+    await ISTKHAR.stop()
+    LOGGER.info("❌ ISTKHAR MUSIC BOT STOPPED")
